82ee6c450dabb923ff3aaab5575f8c29c94f8dbb
mikedimeji <oladimeji.michael12345@gmail.com>
2024-10-06 22:56:43 +0100

    proxy change

--- frontend/src/services/nasaService.js
+++ frontend/src/services/nasaService.js
@@ -5,6 +5,10 @@ const API_URL =
     ? `${process.env.REACT_APP_BACKEND_URL}/api/mars-rover-photos`  // Backend URL from Render
     : '/api/mars-rover-photos';  // Local development URL
 
+    
+console.log('Backend URL:', process.env.REACT_APP_BACKEND_URL);
+
+
 export const getMarsRoverPhotos = async ({ sol, earth_date, rover, page = 1, limit = 10 }) => {
   try {
     const response = await axios.get(API_URL, {
