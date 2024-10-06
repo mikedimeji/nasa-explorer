5cf0fb6b47b31ab451f9271ece66549100227b6b
mikedimeji <oladimeji.michael12345@gmail.com>
2024-10-06 22:40:17 +0100

    More Cors Changes

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,14 @@
 require('dotenv').config(); 
-const cors = require('cors');
 const express = require('express');
 const marsRoverRoutes = require('./routes/marsRoverRoutes');
 const epicRoutes = require('./routes/epicRoutes'); // Import the epic routes
-const axios = require('axios');  // For making HTTP requests
 
 const app = express();
 
-app.use(express.json()); // Middleware to parse JSON requests
+// Middleware
 app.use('/api', marsRoverRoutes); // Mars Rover routes
 app.use('/api', epicRoutes); // EPIC routes
-app.use(cors());
-
 
 module.exports = app;
+
 
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,22 @@
 require('dotenv').config();
 const cors = require('cors');
+const express = require('express');
 const app = require('./app');
 const port = process.env.PORT || 10000;
 
-
+// CORS configuration
 const corsOptions = {
-  origin: ['https://nasa-explorer-1.onrender.com'],
-  methods: 'GET', 
-  credentials: true, 
+  origin: ['https://nasa-explorer-1.onrender.com'], // Allowed origin
+  methods: 'GET',
+  credentials: true,
 };
 
-app.use(cors(corsOptions));
+// Middleware
+app.use(cors(corsOptions)); // Use CORS options
+app.use(express.json()); // Middleware to parse JSON requests
 
+// Start server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
-  console.log("NASA API Key:", process.env.NASA_API_KEY);
 });
+
