d9f9af3383f64f284715f5565ec8cbc195335429
mikedimeji <oladimeji.michael12345@gmail.com>
2024-10-07 16:36:46 +0100

    more cors changes

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,11 @@ const corsOptions = {
 
 // Middleware
 app.use(cors(corsOptions));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.json()); // Middleware to parse JSON requests
 
 // Start server
